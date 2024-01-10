95ddd2be73ce7cefd7c852d3d4a4b86a366e1d10
Mike Maley <michael.r.maley@gmail.com>
2024-01-09 22:17:19 -0500

    open file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::file;
+use std::fs;
 
 mod day1;
 
 fn main() {
-    let file_path = "input/day1.txt";
-    let contents = fs::read_to_string(file_path);
+    let file_path = "./inputs/day1.txt";
+    let contents = fs::read_to_string(file_path).expect("Please provide valid file name");
     println!("{contents}");
 }
