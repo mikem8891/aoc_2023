d3c040b4db27456fe673c2e000db66517ca807f8
Mike Maley <michael.r.maley@gmail.com>
2024-01-11 20:36:07 -0500

    update src/day2.rs

--- src/day2.rs
+++ src/day2.rs
@@ -32,7 +32,16 @@
  
 pub fn snow_island_game_p2(game_rec: &str) -> u64 {
     let mut sum = 0;
-    
+    for game in game_rec.lines() {
+        let mut game = game.split(':');
+        let game_id: u64 = game.next().unwrap()[5..].parse().unwrap();
+        let subsets_of_cubes = game.next().unwrap();
+        let mut max_colors = [0, 0, 0];
+        for subset_of_cubes in subsets_of_cubes.split(';') {
+            for num_color in subset_of_cubes.split(',') {
+            }
+        }
+    }
     sum
 }
  
@@ -40,5 +49,15 @@ pub fn snow_island_game_p2(game_rec: &str) -> u64 {
 mod test{
     #[test]
     fn example(){
+    }
+    
+    fn example_2(){
+        let input =
+r#"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"#;
+        assert_eq!(2286, snow_island_game_p2(input))
     }
 }
