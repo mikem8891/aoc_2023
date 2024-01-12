ed1030826391e67cb372df22d87d164d8dd7cfa2
Mike Maley <michael.r.maley@gmail.com>
2024-01-12 17:32:26 -0500

    update src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let contents = fs::read_to_string(file_path)
         .expect("Day 1 input not found");
     println!("part 1: sum is {}", day1::trebuchet_calibration(&contents));
-    println!("part 2: sum is {}", day1::trebuchet_calibration_p2(&contents));
+    println!("part 2: sum is {}", day1::trebuchet_calibration_p2(&contents).unwrap());
     println!("Day 2");
     let file_path = "./inputs/day2.txt";
     let contents = fs::read_to_string(file_path)
