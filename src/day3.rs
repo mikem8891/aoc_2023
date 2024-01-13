19c9ae97b99d412ab5cac6647715ac6c10cf2741
Mike Maley <michael.r.maley@gmail.com>
2024-01-12 21:16:57 -0500

    update src/day3.rs

--- src/day3.rs
+++ src/day3.rs
@@ -1,3 +1,5 @@
+use crate::util::*;
+
 pub fn part_nums(eng_schem: &str) -> u64 {
     let mut sum = 0;
     let eng_schem: Vec<&str> = eng_schem.lines().collect();
@@ -6,7 +8,10 @@ pub fn part_nums(eng_schem: &str) -> u64 {
         r >= 0 && r < eng_schem.len()
     }
     for num_row in 0..eng_schem.len() {
-        for num_col in eng_schem[num_row].find()
+        let start = 0;
+        while let Some(num_col) = eng_schem[num_row][start..].find(is_digit) {
+            
+        }
     }
     
     sum
