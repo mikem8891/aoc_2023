8ac8f0b73afe0aa356b0d57a1bbd1f7073c216d8
Mike Maley <michael.r.maley@gmail.com>
2024-01-13 17:58:34 -0500

    update src/day3.rs

--- src/day3.rs
+++ src/day3.rs
@@ -7,35 +7,27 @@ pub fn part_nums(eng_schem: &str) -> u64 {
         c >= 0 && (c as usize) < eng_schem[r].len() &&
         r >= 0 && (r as usize) < eng_schem.len()
     };
-    let is_part_at = |r, c| {
-        is_in_range(r, c) &&
-        eng_schem[r].as_bytes()[c] != b'.';
-    };
     for num_row in 0..eng_schem.len() {
         let mut end = 0;
-        while let Some(start) = eng_schem[num_row][end..].find(is_digit) 'num: {
+        'num: while let Some(start) = eng_schem[num_row][end..].find(is_digit) {
             end = start + eng_schem[num_row][start..].find(is_not_digit).unwrap();
-            let is_part = 'find: {
-                let ranges: [(isize, Range<isize>); 4] = [
-                    (num_row - 1, (start - 1)..(end + 1)),
-                    (num_row    , (start - 1)..(start  )),
-                    (num_row    , (end      )..(end + 1)),
-                    (num_row + 1, (start - 1)..(end + 1))
-                ];
-                for (r, c_range) in ranges {
-                    for c in c_range {
-                        if is_in_range(r, c) {
-                            let (r, c) = (r as usize, c as usize);
-                            if eng_schem[r].as_bytes()[c] != b'.' {
-                                break 'find true;
-                            }
+            let ranges: [(isize, Range<isize>); 4] = [
+                (num_row - 1, (start - 1)..(end + 1)),
+                (num_row    , (start - 1)..(start  )),
+                (num_row    , (end      )..(end + 1)),
+                (num_row + 1, (start - 1)..(end + 1))
+            ];
+            for (r, c_range) in ranges {
+                for c in c_range {
+                    if is_in_range(r, c) {
+                        let (r, c) = (r as usize, c as usize);
+                        if eng_schem[r].as_bytes()[c] != b'.' {
+                            sum += eng_schem[num_row][start..end]
+                                .parse::<u64>().unwrap();
+                            continue 'num;
                         }
                     }
                 }
-                false
-            };
-            if is_part {
-                sum += eng_schem[num_row][start..end].parse::<u64>().unwrap();
             }
         }
     }
