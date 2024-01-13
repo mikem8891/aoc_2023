2b63331277d4ad159e74df0ef1f9a8f6042c365e
Mike Maley <michael.r.maley@gmail.com>
2024-01-13 08:02:53 -0500

    update src/day3.rs

--- src/day3.rs
+++ src/day3.rs
@@ -3,24 +3,29 @@ use crate::util::*;
 pub fn part_nums(eng_schem: &str) -> u64 {
     let mut sum = 0;
     let eng_schem: Vec<&str> = eng_schem.lines().collect();
-    let in_range = |r: usize, c: usize| { 
+    let is_in_range = |r: usize, c: usize| { 
         c >= 0 && c < eng_schem[r].len() &&
         r >= 0 && r < eng_schem.len()
+    };
+    let is_part_at = |r, c| {
+        is_in_range(r, c) &&
+        eng_schem[r].as_bytes()[c] != b'.')
     };
     for num_row in 0..eng_schem.len() {
         let mut end = 0;
         while let Some(start) = eng_schem[num_row][end..].find(is_digit) {
             end = start + eng_schem[num_row][start..].find(is_not_digit).unwrap();
             let mut part = 'find: {
+                
                 for c in ((start - 1)..(end + 1)).filter(|c| in_range(num_row - 1, *c)) {
                     if eng_schem[num_row - 1].as_bytes()[c] != b'.' {
                         break 'find true;
                     }
                 }
-                if in_range(num_row, start - 1) && eng_schem[num_row].as_bytes()[start - 1] != b'.' {
-                    break 'find true;
-                }
-                if in_range(num_row, end) && eng_schem[num_row].as_bytes()[end] != b'.' {
+                if (in_range(num_row, start - 1) && 
+                    eng_schem[num_row].as_bytes()[start - 1] != b'.') ||
+                    (in_range(num_row, end) && 
+                    eng_schem[num_row].as_bytes()[end] != b'.') {
                     break 'find true;
                 }
                 false
