50eaddc66445d67cad7559b896fae1dc75fbaadd
Mike Maley <michael.r.maley@gmail.com>
2024-01-12 21:07:09 -0500

    update src/day3.rs

--- src/day3.rs
+++ src/day3.rs
@@ -1,8 +1,14 @@
 pub fn part_nums(eng_schem: &str) -> u64 {
     let mut sum = 0;
-    for line in eng_schem.lines() {
-        
+    let eng_schem: Vec<&str> = eng_schem.lines().collect();
+    let in_range = |r, c| { 
+        c >= 0 && c < eng_schem[r].len() &&
+        r >= 0 && r < eng_schem.len()
     }
+    for num_row in 0..eng_schem.len() {
+        for num_col in eng_schem[num_row].find()
+    }
+    
     sum
 }
 
