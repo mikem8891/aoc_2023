18715e789109cd6ccc530b64de24cd60e28a39d9
Mike Maley <michael.r.maley@gmail.com>
2024-01-14 16:22:48 -0500

    update src/day4.rs

--- src/day4.rs
+++ src/day4.rs
@@ -2,11 +2,16 @@ use std::collections::HashSet;
 //use crate::util::*;
 
 pub fn solve(input: &str) -> (u64, u64) {
-    let (mut sum_1, mut sum_2)  = (0, 0);
-    let game_cards = HashMap::new();
+    let mut sum_1  = 0;
+    let mut game_cards = HashMap::new::<u64, u64>();
     for game in input.lines() {
-        let game_id: u64 = game.next().unwrap()[5..].parse().unwrap()
-        let mut nums = game.split(':').next().unwrap().split('|');
+        let game = game.split(':');
+        let game_id: u64 = game.next().unwrap()
+            .split(' ').last().unwrap()
+            .parse().unwrap();
+        game_card_num = game_cards.entry(game_id).or_insert(0);
+        *game_card_num += 1;
+        let mut nums = game..next().unwrap().split('|');
         let win_nums = nums.next().unwrap()
             .split(' ').filter(|s| !s.is_empty())
             .map(|s| s.parse::<u64>().unwrap())
@@ -15,12 +20,17 @@ pub fn solve(input: &str) -> (u64, u64) {
             .split(' ').filter(|s| !s.is_empty())
             .map(|s| s.parse::<u64>().unwrap())
             .filter(|n| win_nums.contains(n))
-            .collect::<Vec<_>>();
-        if !elf_nums.is_empty() {
-            sum_1 += (1 << (elf_nums.len() - 1)) as u64;
+            .collect::<Vec<_>>().len();
+        if elf_nums != 0 {
+            sum_1 += (1 << (elf_nums - 1)) as u64;
         }
-        
+        for new_cards in 1..=elf_nums {
+            let new_card_id = game_id + new_cards;
+            let new_game_card_num = game_cards.entry(new_card_id).or_insert(0);
+            *new_game_card_num += game_card_num;
+        }
     }
+    let sum_2 = game_cards.iter().map(|(k, v)| v).sum();
     (sum_1, sum_2)
 }
 
