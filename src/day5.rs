18bf576ec96a345785882507956eb7b2fd980764
Mike Maley <michael.r.maley@gmail.com>
2024-01-14 20:39:30 -0500

    update src/day5.rs

---
+++ src/day5.rs
@@ -0,0 +1,18 @@
+pub fn solve(input: &str) -> (u64, u64) {
+    let sum_1 = 0;
+    let sum_2 = 0;
+    
+    (sum_1, sum_2)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let input =
+r#""#;
+        assert_eq!((0, 0), solve(input));
+    }
+}
