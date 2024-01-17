e7b2ba3565a5bd52972664247026127b75c69bae
Mike Maley <michael.r.maley@gmail.com>
2024-01-16 20:02:49 -0500

    update src/day5/test.rs

---
+++ src/day5/test.rs
@@ -0,0 +1,15 @@
+const EXAMPLE_SOLUTION: [&str; 2] = [todo!()];
+
+#[test]
+fn example_1(){
+    let path = format!("./src/day{DAY_NUM}/example_1.txt");
+    let input = std::fs::read_to_string(path).unwrap();
+    assert_eq!(solve(input)[0], EXAMPLE_SOLUTION[0]); 
+}
+
+#[test]
+fn example_2(){
+    let path = format!("./src/day{DAY_NUM}/example_2.txt");
+    let input = std::fs::read_to_string(path).unwrap();
+    assert_eq!(solve(input)[1], EXAMPLE_SOLUTION[1]); 
+}
