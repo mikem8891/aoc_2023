0c6065ac9f8e05dfd310b6b2fc674883aca52a6a
Mike Maley <michael.r.maley@gmail.com>
2024-01-16 19:42:22 -0500

    update src/day5.rs

--- src/day5.rs
+++ src/day5.rs
@@ -1,18 +1,17 @@
-pub fn solve(input: &str) -> (u64, u64) {
-    let sum_1 = 0;
-    let sum_2 = 0;
-    
-    (sum_1, sum_2)
+const DAY_NUM: &str = "5";
+
+fn solve(input: &str) -> [String; 2] {
+    todo!();
+}
+
+pub fn main() {
+    println!("Day 5");
+    let path = format!("./src/day{DAY_NUM}/input.txt");
+    let input = std::fs::read_to_string(path).unwrap();
+    let [part_1, part_2] = solve(input);
+    println!("part 1 is {part_1}");
+    println!("part 2 is {part_2}");
 }
 
 #[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn example() {
-        let input =
-r#""#;
-        assert_eq!((0, 0), solve(input));
-    }
-}
+mod test;
