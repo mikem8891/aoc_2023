4c5a42c5dd6a6fe75f942e9820e9729959426622
Mike Maley <michael.r.maley@gmail.com>
2024-01-16 20:06:33 -0500

    update src/day5/mod.rs

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -8,7 +8,7 @@ pub fn main() {
     println!("Day 5");
     let path = format!("./src/day{DAY_NUM}/input.txt");
     let input = std::fs::read_to_string(path).unwrap();
-    let [part_1, part_2] = solve(input);
+    let [part_1, part_2] = solve(&input);
     println!("part 1 is {part_1}");
     println!("part 2 is {part_2}");
 }
