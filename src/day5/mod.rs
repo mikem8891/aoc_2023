06c575df0f30e2e5a8b459c1b7f59da58ee69a82
Mike Maley <michael.r.maley@gmail.com>
2024-01-27 11:52:11 -0500

    update src/day5/mod.rs

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -1,10 +1,31 @@
 const DAY_NUM: &str = "5";
 
+struct SeedMap {
+    name: String,
+    maps: Vec<[u64; 3]>
+}
+
+impl SeedMap {
+    fn new(text: &str) -> SeedMap {
+        let (name, nums) = text.split_once(":\n");
+        let mut maps: Vec<[u64; 3]> = vec![];
+        for map in nums.lines() {
+            let mut map = map.split(' ').filter_map(|s| s.parse::<u64>().ok());
+            let get_next = |&mut iter| iter.next().unwrap();
+            let map = [get_next(&map), get_next(&map), get_next(&map)];
+            maps.push(map);
+        }
+        SeedMap {name, maps}
+    }
+}
+
 fn solve(input: &str) -> [String; 2] {
     let (seeds, almanac) = input.split_once("\n\n").unwrap();
     let seeds: Vec<u64> = seeds.split(' ')
         .filter_map(|s| s.parse::<u64>().ok())
         .collect();
+    let seed_maps: Vec<_> = almanac.split("/n/n").map(SeedMap::new).collect();
+    
     todo!();
 }
 
