33c00493c056dd61b47306fee3a3663857d3126e
Mike Maley <michael.r.maley@gmail.com>
2024-01-28 11:11:25 -0500

    update src/day5/mod.rs

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -1,22 +1,35 @@
 const DAY_NUM: &str = "5";
 
-struct SeedMap {
-    name: String,
-    maps: Vec<[u64; 3]>
-}
+use std::collections::HashMap;
+
+struct SeedMap(Vec<[u64; 3]>);
 
 impl SeedMap {
-    fn new(text: &str) -> SeedMap {
-        let (name, nums) = text.split_once(":\n");
-        let mut maps: Vec<[u64; 3]> = vec![];
-        for map in nums.lines() {
-            let mut map = map.split(' ').filter_map(|s| s.parse::<u64>().ok());
-            let get_next = |&mut iter| iter.next().unwrap();
-            let map = [get_next(&map), get_next(&map), get_next(&map)];
-            maps.push(map);
+    fn get(&self, input: u64) -> u64 {
+        let in_range = |sm| 0 <= input && input < sm[2];
+        let result = self.map(|s| input - s[1])
+            .filter(|r| 0 <= input && input < r[2])
+            .map(|d| input + d[0])
+            .next();
+        if let Some(output) = result {
+            output
+        } else {
+            input
         }
-        SeedMap {name, maps}
     }
+}
+
+fn seed_map(text: &str) -> (String, SeedMap) {
+    let (name, nums) = text.split_once(":\n");
+    let mut maps: Vec<[u64; 3]> = vec![];
+    for map in nums.lines() {
+        let mut map = map.split(' ').filter_map(|s| s.parse::<u64>().ok());
+        let get_next = |&mut iter| iter.next().unwrap();
+        let map = [get_next(&map), get_next(&map), get_next(&map)];
+        maps.push(map);
+    }
+    maps.sort_by_key(|&m| m[1]);
+    (name.to_owned(), SeedMap(maps))
 }
 
 fn solve(input: &str) -> [String; 2] {
@@ -24,7 +37,12 @@ fn solve(input: &str) -> [String; 2] {
     let seeds: Vec<u64> = seeds.split(' ')
         .filter_map(|s| s.parse::<u64>().ok())
         .collect();
-    let seed_maps: Vec<_> = almanac.split("/n/n").map(SeedMap::new).collect();
+    let seed_maps: HashMap<_, _> = almanac.split("/n/n")
+        .map(seed_map)
+        .collect();
+    const MAP_ORDER: [&str; ] = [
+        ""
+    ];
     
     todo!();
 }
