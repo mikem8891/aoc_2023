9e50010e39d9cb37a7225bd1d51880dc4d8293ff
Mike Maley <michael.r.maley@gmail.com>
2024-01-28 21:11:56 -0500

    Update mod.rs

--- src/day5/mod.rs
+++ src/day5/mod.rs
@@ -6,20 +6,24 @@ struct SeedMap(Vec<[u64; 3]>);
 
 impl SeedMap {
     fn get(&self, input: u64) -> u64 {
-        self.map(|s| input - s[1])
-            .filter(|r| 0 <= input && input < r[2])
-            .map(|d| input + d[0])
-            .next().unwrap_or(input);
+        self.0.iter().map(|s| (input - s[1], *s))
+            .filter(|(input, r)| 0 <= *input && input < &r[2])
+            .map(|(input, d)| input + d[0])
+            .next().unwrap_or(input)
     }
 }
 
 fn seed_map(text: &str) -> (String, SeedMap) {
-    let (name, nums) = text.split_once(":\n");
+    
+    let (name, nums) = text.split_once(":\n").unwrap();
     let mut maps: Vec<[u64; 3]> = vec![];
     for map in nums.lines() {
         let mut map = map.split(' ').filter_map(|s| s.parse::<u64>().ok());
-        let get_next = |&mut iter| iter.next().unwrap();
-        let map = [get_next(&map), get_next(&map), get_next(&map)];
+        let map = [
+            map.next().unwrap(), 
+            map.next().unwrap(), 
+            map.next().unwrap()
+        ];
         maps.push(map);
     }
     maps.sort_by_key(|&m| m[1]);
@@ -28,20 +32,26 @@ fn seed_map(text: &str) -> (String, SeedMap) {
 
 fn solve(input: &str) -> [String; 2] {
     let (seeds, almanac) = input.split_once("\n\n").unwrap();
-    let mut seeds: Vec<u64> = seeds.split(' ')
+    let seeds: Vec<u64> = seeds.split(' ')
         .filter_map(|s| s.parse::<u64>().ok())
         .collect();
-    let seed_maps: HashMap<_, _> = almanac.split("/n/n")
+    let seed_maps: HashMap<_, _> = almanac.split("\n\r\n\r")
         .map(seed_map)
         .collect();
-    let map_seeds = |s: Vec<u64>, m: SeedMap| 
-        s.map(m.get).collect::<Vec<_>>();
-    const MAP_ORDER: [&str; N] = [
-        ""
+    let map_seeds = |s: Vec<u64>, m: &&SeedMap| 
+        s.iter().map(|s| m.get(*s)).collect::<Vec<_>>();
+    const MAP_ORDER: [&str; 7] = [
+        "seed-to-soil map",
+        "soil-to-fertilizer map",
+        "fertilizer-to-water map",
+        "water-to-light map",
+        "light-to-temperature map",
+        "temperature-to-humidity map",
+        "humidity-to-location map"
     ];
-    let locations = MAP_ORDER.map(|n| seed_maps[n.to_owned])
-        .fold(seeds, map_seeds);
-    [location.min().to_string(), "todo"]
+    let locations = MAP_ORDER.map(|n| &seed_maps[&n.to_owned()])
+        .iter().fold(seeds, map_seeds);
+    [locations.iter().min().unwrap().to_string(), "todo".to_owned()]
 }
 
 pub fn main() {
