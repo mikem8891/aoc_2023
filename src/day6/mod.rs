3803d8400fd44f4545eb9c82823531078fc4b48f
Mike Maley <michael.r.maley@gmail.com>
2024-02-04 11:29:56 -0500

    update src/day6/mod.rs

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,16 +1,34 @@
 const DAY_NUM: &str = "6";
 
 fn solve(input: &str) -> [String; 2] {
-    let lines: Vec<&str> = input.lines().collect();
-    let times: Vec<u32> = lines[0].split_once(':').unwrap().1
-        .split(' ').filter_map(|s| s.parse::<u32>().ok())
-        .collect();
-    let dists: Vec<u32> = lines[1].split_once(':').unwrap().1
-        .split(' ').filter_map(|s| s.parse::<u32>().ok())
-        .collect();
-    
+    let lines: Vec<&str> = input.lines();
+    let times_iter: Vec<u32> = lines.next().unwrap()
+        .split_once(':').unwrap().1
+        .split(' ').filter_map(|s| s.parse::<u32>().ok());
+    let dists_iter: Vec<u32> = lines.next().unwrap()
+        .split_once(':').unwrap().1
+        .split(' ').filter_map(|s| s.parse::<u32>().ok());
+    let races = times_iter.zip(dist_iter).collect();
+    // d = ht * (t - ht)
+    // ht = (t / 2) ± √((t / 2)² - d)
+    let hold_time_rng = |(t, d)| {
+        let hlf_t = (t as f64) / 2;
+        let sq = hlf_t * hlf_t - (d as f64);
+        if sq <= 0 {
+            panic!("no solution for a time of {t} and distance of {d}");
+        }
+        let sqrt = f64::sqrt(rad);
+        let start = hlf_t - sqrt(rad);
+        let end   = hlf_t + sqrt(rad);
+        let start = (start + 1.0).floor() as u32;
+        let end   = (end   - 1.0).ceil()  as u32;
+        (start, end)
+    }
+    let ways_to_win = races.iter().map(hold_time_rng)
+        .map(|(s, e)| e - s + 1)
+        .product();
     [
-        "todo".to_owned(), 
+        ways_to_win.to_string(), 
         "todo".to_owned()
     ]
 }
