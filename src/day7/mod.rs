9d124ab4d2f41c4bdff63f84a3bf15012795bbc7
Mike Maley <michael.r.maley@gmail.com>
2024-02-05 19:34:55 -0500

    update src/day7/mod.rs

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -1,7 +1,101 @@
 const DAY_NUM: &str = "7";
 
-fn solve(input: &str) -> [String; 2] {
+use std::collections::BTreeMap;
 
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+enum HandType {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeKind,
+    FullHouse,
+    FourKind,
+    FiveKind
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+enum Card {
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
+    Ten = 10,
+    Jack = 11,
+    Queen = 12,
+    King = 13,
+    Ace = 14
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct Hand {
+    hand_type: HandType,
+    cards: [Card; 5]
+}
+
+impl HandType {
+    fn new(cards: &[Cards; 5]) -> HandType {
+        let mut count = BTreeMap::<_,u8>::new();
+        for card in cards.iter() {
+            count.entry(card)
+                .and_modify(|c| *c += 1)
+                .or_insert(1);
+        }
+        let mut count: Vec<_> = count.into_iter()
+            .map(|_, v| v)
+            .collect();
+        count.sort();
+        use HandType::*;
+        match count[..] {
+            [5] => FiveKind,
+            [.., 4] => FourKind,
+            [2, 3] => FullHouse,
+            [.., 3] => ThreeKind,
+            [.., 2, 2] => TwoPair,
+            [.., 2] => OneKind,
+            [..] => HighCard
+        }
+    }
+}
+
+impl Card {
+    fn new(c: char) -> Card {
+        use Card::*;
+        match c {
+            '2' => Two,
+            '3' => Three,
+            '4' => Four,
+            '5' => Five,
+            '6' => Six,
+            '7' => Seven,
+            '8' => Eight,
+            '9' => Nine,
+            'T' => Ten,
+            'J' => Jack,
+            'Q' => Queen,
+            'K' => King,
+            'A' => Ace,
+            c => panic!("'{c}' is an invalid char for a Card")
+        }
+    }
+}
+
+impl Hand {
+    fn new(text: &str) -> Hand {
+        let mut cards = [Card::Ace; 5];
+        for (pos, card) in text.chars().enumerate() {
+            cards[pos] = Card::new(card);
+        }
+        let hand_type = HandType::new(&cards);
+        Hand {hand_type, cards}
+    }
+}
+
+fn solve(input: &str) -> [String; 2] {
+    
     
     [
         "todo".to_string(), 
