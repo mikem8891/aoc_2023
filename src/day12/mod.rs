d8e02d3689e21e102a55b7dc8bea1adec497fff0
Mike Maley <michael.r.maley@gmail.com>
2024-02-17 22:46:24 -0500

    update src/day12/mod.rs

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -1,8 +1,42 @@
 const DAY_NUM: &str = "12";
 
+enum Condition {
+    Operational, Damaged, Unknown
+}
+
+impl From<u8> for Condition {
+    fn from(cond: u8) -> Self {
+        use Condition as C;
+        match cond{
+            b'.' => C::Operational,
+            b'#' => C::Damaged,
+            b'?' => C::Unknown,
+            err => panic!("expected '.', '#', '?'; found '{}'", err as char)
+        }
+    }
+}
+
+fn permutations(line: &str) -> u32 {
+    let (rec, dam_gp) = line.split_once(' ')
+        .expect("no space on line");
+    
+    let dam_gp: Vec<u8> = dam_gp.split(',')
+        .map(|n| n.parse::<u8>().unwrap())
+        .collect();
+    
+    let total_dam = dam_gp.iter().sum();
+    let known_dam = rec.bytes()
+        .filter(|c| c == b'#').count();
+    let unknown_dam = total_dam - known_dam;
+    
+    todo!();
+}
+
 fn solve(input: &str) -> [String; 2] {
+    let total_permutations = input.lines().map(permutations).sum();
+    
     [
-        "todo".to_string(),
+        total_permutations.to_string(),
         "todo".to_string()
     ]
 }
