fe79915782677cd26f6266d2709e5b2f38e72871
Mike Maley <michael.r.maley@gmail.com>
2024-02-20 21:39:14 -0500

    day 12 p1

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -10,91 +10,71 @@ fn permutations(line: &str) -> u32 {
         .map(|n| n.parse::<u8>().unwrap())
         .collect();
     
-    let total_dam = dam_gp.iter().sum();
+    let total_dam: u8 = dam_gp.iter().sum();
     let known_dam = rec.bytes()
-        .filter(|c| *c == b'#').count();
+        .filter(|c| *c == b'#').count() as u8;
     let unknown_dam = total_dam - known_dam;
     
     let unknown_idx: Vec<_> = rec.match_indices('?')
         .map(|(i, _)| i).collect();
-    let unknowns = unknown_idx.len();
+    let unknowns = unknown_idx.len() as u8;
     let unknown_op = unknowns - unknown_dam;
     
     let mut trial_rec = Vec::from(rec);
-    let mut count = 0;
-    let (mut dam_cnt, mut op_cnt) = (0, 0);
-    fn is(that: u8) {|this: u8| this == that}
-    struct State{
-        dam_gp: &[u8],
+    struct State<'a>{
+        dam_gp: &'a [u8],
         dam_rem:  u8,
         op_rem:   u8
     }
     fn perm_recur(rec: &mut [u8], state: &mut State) -> u32 {
-        let i = rec.iter().position(|c| c == b'?');
+        let i = rec.iter().position(|c| *c == b'?');
         if let Some(i) = i {
             // create permutations
-            if *state.dam_rem > 0 {
-                *dam_rem -= 1;
+            let mut count = 0;
+            if state.dam_rem > 0 {
+                state.dam_rem -= 1;
                 rec[i] = b'#';
-                perm_recur(rec, state);
-                *dam_rem += 1;
+                count += perm_recur(rec, state);
+                state.dam_rem += 1;
                 rec[i] = b'?';
             }
-            if op_rem > 0 {
-                *op_rem -= 1;
+            if state.op_rem > 0 {
+                state.op_rem -= 1;
                 rec[i] = b'.';
-                perm_recur(rec, state);
-                *op_rem += 1;
+                count += perm_recur(rec, state);
+                state.op_rem += 1;
                 rec[i] = b'?';
             }
-            assert!(dam_rem > 0 || op_rem > 0);
+            assert!(state.dam_rem > 0 || state.op_rem > 0);
+            count
         } else {
             // check permutation
-            if 
+            if check(rec, state.dam_gp) {1} else {0}
         }
     }
-    let check = |i: usize| {
-        let of = |a: u8| {|b: u8| a == b};
-        let i = trial_rec.iter().position(of(b'?'));
-        if let Some(i) = i {
-            assert!(op_cnt < unknown_op  || dam_cnt < unknown_dam);
-            if op_cnt < unknown_op {
-                unsafe{*trial_rec.as_bytes_mut()[i] = b'.';}
-                op_cnt += 1;
-                check(i + 1);
-                op_cnt -= 1;
-            }
-            if dam_cnt < unknown_dam {
-                unsafe{*trial_rec.as_bytes_mut()[i] = b'#';}
-                dam_cnt += 1;
-                check(i + 1);
-                dam_cnt -= 1;
-            }
-            unsafe{*trial_rec.as_bytes_mut()[i] = b'?';}
-        } else {
-            assert!(trial_rec.find('?').is_none());
-            let mut i = 0;
-            let trail_dam_gp = vec![];
-            while let Some(j_s) = trial_rec[i..].find('#') {
-                let j_e = trial_rec[j_s..].find('.')
-                    .unwrap_or(trial_rec.len());
-                trail_dam_gp.push(j_e - j_s);
-            }
-            assert!(trail_dam_gp.sum() == total_dam);
-            if dam_gp.iter().zip(&trail_dam_gp).all(|(a, b)| a == b) {
-                count += 1;
-            }
+    fn check(rec: &[u8], dam_gp: &[u8]) -> bool {
+        assert!(rec.iter().position(|c| *c == b'?').is_none());
+        let mut i = 0;
+        let mut trail_dam_gp = vec![];
+        while let Some(j_s) = rec[i..].iter().position(|c| *c == b'#') {
+            let j_s = j_s + i;
+            let j_e = j_s + rec[j_s..].iter().position(|c| *c == b'.')
+                .unwrap_or(rec.len() - j_s);
+            i = j_e;
+            assert!(j_e > j_s, "j_e > j_s, {j_e} > {j_s}");
+            trail_dam_gp.push((j_e - j_s) as u8);
         }
-    };
-    check(0);
-    count
+        dam_gp.iter().zip(&trail_dam_gp).all(|(a, b)| a == b)
+    }
+    let mut state = State {dam_gp: &dam_gp, dam_rem: unknown_dam, op_rem: unknown_op};
+    perm_recur(&mut trial_rec, &mut state)
 }
 
 fn solve(input: &str) -> [String; 2] {
-    let total_permutations = input.lines().map(permutations).sum();
+    let total_permutations: u32 = input.lines().map(permutations).sum();
     
     [
-        "todo".to_string(),
+        total_permutations.to_string(),
         "todo".to_string()
     ]
 }
